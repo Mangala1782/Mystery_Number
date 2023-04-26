b1a4c5f21abff971f6cf79110002c7f48360f0fc
Dr. Plague <38175344+Mangala1782@users.noreply.github.com>
2023-04-26 12:27:43 -0500

    Update Mystery Number.py

--- Mystery Number.py
+++ Mystery Number.py
@@ -1,7 +1,6 @@
 # Date: 04 - 26 - 2023
 # Name: Xaiver Hickey
 # Project Name: Number Mystery 
-# Skill demostration: Logic, User input, and random number generation
 
 
 # Imports 
@@ -9,7 +8,6 @@ import random
 
 # Var
 attempts = 0
-win = 0 
 
 # Number generator
 number = random.randint(1,100)
@@ -24,18 +22,19 @@ while (userNum != number):
         attempts += 1
     
     elif userNum == number: # Victory
-        message = "You guessed the number!"
-        win += 1
+        print("==][================][==")
+        print("You guessed the number!")
+        print("==][================][==")
+        input('Press ENTER to exit')
     
     else: # Too low 
         message = "Number too low!"
         attempts += 1
   
-        print()
-        print(message)
+    print()
+    print(message)
 
     # Display attempts
-        print()
-        print("Attempts Made: ", attempts)
-        print("==][================][==")
-
+    print()
+    print("Attempts Made: ", attempts)
+    print("==][================][==")
