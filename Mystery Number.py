45d5b80108467cd919fc8b1bc8f2984236b4f707
Dr. Plague <38175344+Mangala1782@users.noreply.github.com>
2023-04-29 01:19:06 -0500

    Update Mystery Number.py

--- Mystery Number.py
+++ Mystery Number.py
@@ -1,40 +1,71 @@
 # Date: 04 - 26 - 2023
+# Finished: 04 - 29 - 2023
 # Name: Xaiver Hickey
 # Project Name: Number Mystery 
 
 
-# Imports 
 import random
 
-# Var
-attempts = 0
+# Game Menu
+def menu():
+    play_again = True
 
-# Number generator
-number = random.randint(1,100)
-userNum = 0
+    # Difficulty scale.
+    while play_again:
+        print("Welcome to Mysterious Number!")
+        print("1. Easy (1-10)")
+        print("2. Medium (1-50)")
+        print("3. Hard (1-100)")
+        print("4. Quit")
+        print("==][====================================][==")
+        choice = input("Enter your choice (1-4): ")
 
-while (userNum != number):
-    #input 
-    userNum = int(input("Please guess a number from 1 to 100: "))
+        # game(1, 10, 3) 1 is the starting number, and 10, 50, 100 is the maximum number. 
+        # 3, 5, 7 is the number of guesses allowed. 
+        if choice == "1":
+            game(1, 10, 3)
+        elif choice == "2":
+            game(1, 50, 5)
+        elif choice == "3":
+            game(1, 100, 7)
 
-    if userNum > number: # Too high
-        message = "Number too high!"
-        attempts += 1
-    
-    elif userNum == number: # Victory
-        print("==][================][==")
-        print("You guessed the number!")
-        print("==][================][==")
-        input('Press ENTER to exit')
-    
-    else: # Too low 
-        message = "Number too low!"
-        attempts += 1
-  
-    print()
-    print(message)
+        # Reply with 4 to break loop. 
+        elif choice == "4":
+            play_again = False
+            print("Thanks for playing!")
+        
+        # Fail safe error to help user not to break the application.
+        else:
+            print("Invalid choice. Try again.")
 
-    # Display attempts
-    print()
-    print("Attempts Made: ", attempts)
-    print("==][================][==")
+# Main Game Function
+def game(min_num, max_num, max_guesses):
+    num = random.randint(min_num, max_num)
+    num_guesses = 0
+
+    # Loop function. 
+    while num_guesses < max_guesses:
+
+        try: # Informs the player the two num. The var changes based on difficulty.
+            guess = int(input(f"Guess a number between {min_num} and {max_num}: "))
+            num_guesses += 1
+
+            if guess < num: # This gives player a hint if the num too low. Subtract -1 guess
+                print("Too low, try again.")
+
+            elif guess > num: # This gives player a hint if the num too high. Subtract -1 guess
+                print("Too high, try again.")
+
+            else: # This rewards the player with a victory. Informing how many guesses it took.
+                print(f"Congratulations, you guessed the number in {num_guesses} guesses!")
+                return # Return to menu
+            
+        # Fail safe incase invalid input won't break it.
+        except ValueError: 
+            print("Invalid input. Try again.")
+
+    print("===][=========================================][===")
+    print(f"Game Over... The number was {num}.")
+
+if __name__ == "__main__":
+    menu()
